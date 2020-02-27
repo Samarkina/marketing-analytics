7f36c1244661b71775a221a763cad473d48e5ac7
Ekaterina Samarkina <sea94@inbox.ru>
2020-02-26 18:29:55 -0800

    Complete Task 2.2

--- src/main/scala/com/samarkina/bigdata/marketing/ChannelsPerformance.scala
+++ src/main/scala/com/samarkina/bigdata/marketing/ChannelsPerformance.scala
@@ -1,8 +1,11 @@
 package com.samarkina.bigdata.marketing
 
+import org.apache.spark.sql.functions.{avg, col}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
+// Task 2.2
 object ChannelsPerformance {
+
   def highestAmountPlainSQL(spark: SparkSession, purchaseDf: DataFrame, mobileAppClick2: DataFrame) = {
     purchaseDf.createOrReplaceTempView("Purchases")
     mobileAppClick2.createOrReplaceTempView("Clicks")
@@ -18,4 +21,23 @@ object ChannelsPerformance {
         |GROUP BY channel_id
         |""".stripMargin).show()
   }
+
+  def highestAmountDataFrame(spark: SparkSession, purchaseDf: DataFrame, mobileAppClick2: DataFrame) = {
+    val purchaseDfasPurchases = purchaseDf.as("Purchases")
+    val mobileAppClickasClicks = mobileAppClick2.as("Clicks")
+
+    import spark.implicits._
+    val innerTable = mobileAppClickasClicks
+      .select("channel_id", "sessionId")
+      .distinct()
+
+    val highestAmountTable = innerTable
+      .select("channel_id")
+      .groupBy("channel_id")
+      .count()
+
+    highestAmountTable.show()
+
+  }
+
 }
--- src/main/scala/com/samarkina/bigdata/marketing/MarketingAnalyticsApp.scala
+++ src/main/scala/com/samarkina/bigdata/marketing/MarketingAnalyticsApp.scala
@@ -98,14 +98,17 @@ object MarketingAnalyticsApp {
 
 
     // Task 1.1
-//    getTargetTable(purchaseDf, mobileAppClick2)
+    getTargetTable(purchaseDf, mobileAppClick2)
 
     // Task 2.1
-//    TopCampaigns.averagePlainSQL(spark, purchaseDf, mobileAppClick2)
-//
-//    TopCampaigns.averageDataFrame(spark, purchaseDf, mobileAppClick2)
+    TopCampaigns.averagePlainSQL(spark, purchaseDf, mobileAppClick2)
 
+    TopCampaigns.averageDataFrame(spark, purchaseDf, mobileAppClick2)
+
+    // Task 2.2
     ChannelsPerformance.highestAmountPlainSQL(spark, purchaseDf, mobileAppClick2)
+
+    ChannelsPerformance.highestAmountDataFrame(spark, purchaseDf, mobileAppClick2)
 
 
 
