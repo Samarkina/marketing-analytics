0d4316ba0c00ccfd40b764d37dc857a3777d6251
Ekaterina Samarkina <sea94@inbox.ru>
2020-02-26 16:10:18 -0800

    Complete Task 1.1

--- src/main/scala/com/samarkina/bigdata/marketing/MarketingAnalyticsApp.scala
+++ src/main/scala/com/samarkina/bigdata/marketing/MarketingAnalyticsApp.scala
@@ -7,6 +7,26 @@ import org.apache.spark.sql.functions._
 
 
 object MarketingAnalyticsApp {
+
+
+  def campaignChannelSetter = {
+    var channel_id_global = ""
+    (campaign_channel_id: String) => {
+      if (campaign_channel_id != null)
+        channel_id_global = campaign_channel_id
+      channel_id_global
+    }
+  }
+
+  def sessionIdSetter = {
+    var inc = 0
+    (eventType: String, userId: String) => {
+      if (eventType == "app_open")
+        inc += 1
+      userId + "_s" + inc
+    }
+  }
+
   def main(args: Array[String]): Unit = {
 
     val spark = SparkSession
@@ -24,7 +44,6 @@ object MarketingAnalyticsApp {
       .option("header", "true")
       .csv("src/main/resources/mobile-app-clickstream_sample-mobile-app-clickstream_sample.csv")
 
-
     val schemaAppOpen = StructType(Array(
       StructField("campaign_id",StringType,true),
       StructField("channel_id",StringType,true)
@@ -34,25 +53,46 @@ object MarketingAnalyticsApp {
       StructField("purchase_id",StringType,true)
     ))
 
-
     val dfFromCSVJSON =  dfFromCSV.select(col("userId"), col("eventId"), col("eventTime"), col("eventType"),
       from_json(col("attributes"),schemaAppOpen).as("jsonData"),
       from_json(col("attributes"),schemaPurchase).as("jsonData2")
     )
       .select("userId","eventId", "eventTime", "eventType", "jsonData.*", "jsonData2.*")
-    dfFromCSVJSON.printSchema()
-    dfFromCSVJSON.show(false)
 
-    var inc = 0
-    val coder = (eventType: String, userId: String) => {
-      if (eventType == "app_open")
-        inc += 1
-      userId + "_s" + inc
-    }
-    val sqlfunc = spark.udf.register("coders", coder)
+    val sessionIdSet = spark.udf.register("sessionIdSetter", sessionIdSetter).asNondeterministic()
 
-    dfFromCSVJSON.withColumn("sessionId", sqlfunc(col("eventType"), col("userId"))).show()
+    val mobileAppClick = dfFromCSVJSON.withColumn("sessionId", sessionIdSet(col("eventType"), col("userId")))
 
+    val channelSetter = spark.udf.register("channelSetter", campaignChannelSetter).asNondeterministic()
 
+    val campaignSetter = spark.udf.register("campaignSetter", campaignChannelSetter).asNondeterministic()
+
+    val mobileAppClick2 = mobileAppClick
+      .withColumn("channel_id", channelSetter(col("channel_id")))
+      .withColumn("campaign_id", campaignSetter(col("campaign_id")))
+
+    val purchaseDf = spark.read
+      .option("escape", "\"")
+      .option("header", "true")
+      .csv("src/main/resources/purchases_sample-purchases_sample.csv")
+      .withColumn("billingCost", 'billingCost.cast(DoubleType))
+      .as[Purchase]
+
+    val target = purchaseDf.join(
+      mobileAppClick2,
+      col("purchaseId") === col("purchase_id"),
+      "left"
+    )
+      .filter("purchaseId IS NOT NULL")
+
+    target.select(
+      col("purchaseId"),
+      col("purchaseTime"),
+      col("billingCost"),
+      col("isConfirmed"),
+      col("sessionId"),
+      col("campaign_id"),
+      col("channel_id")
+    ) .show(100)
   }
 }
